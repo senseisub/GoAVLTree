ac7128b0655c581ce01eec838c89bccd212b52d9
senseisub <seunsuberu@lyft.com>
2021-06-16 13:00:23 -0700

    Support for Int, Float, and String for AVLTree

--- AVLTree.go
+++ AVLTree.go
@@ -1,11 +1,11 @@
 package main
 
-import(
+import (
 	"bufio"
-  	"fmt"
-  	"os"
-  	"strings"
+	"fmt"
+	"os"
 	"strconv"
+	"strings"
 )
 
 type Generic interface {
@@ -13,62 +13,220 @@ type Generic interface {
 }
 
 type TreeNode struct {
-	data int
-	left *TreeNode
-	right *TreeNode
+	data   interface{}
+	left   *TreeNode
+	right  *TreeNode
 	height int
 }
 
 type AVLTree struct {
 	root *TreeNode
 	size int
-
 }
 
 // height of the tree
-func height(currentNode *TreeNode) int{
+func height(currentNode *TreeNode) int {
 	if currentNode == nil {
-		return 0;
+		return 0
 	}
-	return currentNode.height;
+	return currentNode.height
 }
 
 // A utility function to get maximum
 // of two integers
-func max(a, b int) int{
+func max(a, b int) int {
 	if a > b {
 		return a
-	} 
-	return b;
+	}
+	return b
+}
+
+func (node *TreeNode) GreaterThan(data2 interface{}) bool {
+	data := node.data
+	switch (node.data).(type) {
+	case int:
+		if data.(int) > data2.(int) {
+			return true
+		} else {
+			return false
+		}
+	case int8:
+		if data.(int8) > data2.(int8) {
+			return true
+		} else {
+			return false
+		}
+	case int16:
+		if data.(int16) > data2.(int16) {
+			return true
+		} else {
+			return false
+		}
+	case int32:
+		if data.(int32) > data2.(int32) {
+			return true
+		} else {
+			return false
+		}
+	case int64:
+		if data.(int64) > data2.(int64) {
+			return true
+		} else {
+			return false
+		}
+	case uint8:
+		if data.(uint8) > data2.(uint8) {
+			return true
+		} else {
+			return false
+		}
+	case uint16:
+		if data.(uint16) > data2.(uint16) {
+			return true
+		} else {
+			return false
+		}
+	case uint32:
+		if data.(uint32) > data2.(uint32) {
+			return true
+		} else {
+			return false
+		}
+	case uint64:
+		if data.(uint64) > data2.(uint64) {
+			return true
+		} else {
+			return false
+		}
+	case float64:
+		if data.(float64) > data2.(float64) {
+			return true
+		} else {
+			return false
+		}
+	case float32:
+		if data.(float32) > data2.(float32) {
+			return true
+		} else {
+			return false
+		}
+	case string:
+		if data.(string) > data2.(string) {
+			return true
+		} else {
+			return false
+		}
+	}
+	return false
+}
+
+func (node *TreeNode) LesserThan(data2 interface{}) bool {
+	data := node.data
+	switch (node.data).(type) {
+	case int:
+		if data.(int) < data2.(int) {
+			return true
+		} else {
+			return false
+		}
+	case int8:
+		if data.(int8) < data2.(int8) {
+			return true
+		} else {
+			return false
+		}
+	case int16:
+		if data.(int16) < data2.(int16) {
+			return true
+		} else {
+			return false
+		}
+	case int32:
+		if data.(int32) < data2.(int32) {
+			return true
+		} else {
+			return false
+		}
+	case int64:
+		if data.(int64) < data2.(int64) {
+			return true
+		} else {
+			return false
+		}
+	case uint8:
+		if data.(uint8) < data2.(uint8) {
+			return true
+		} else {
+			return false
+		}
+	case uint16:
+		if data.(uint16) < data2.(uint16) {
+			return true
+		} else {
+			return false
+		}
+	case uint32:
+		if data.(uint32) < data2.(uint32) {
+			return true
+		} else {
+			return false
+		}
+	case uint64:
+		if data.(uint64) < data2.(uint64) {
+			return true
+		} else {
+			return false
+		}
+	case float64:
+		if data.(float64) < data2.(float64) {
+			return true
+		} else {
+			return false
+		}
+	case float32:
+		if data.(float32) < data2.(float32) {
+			return true
+		} else {
+			return false
+		}
+	case string:
+		if data.(string) < data2.(string) {
+			return true
+		} else {
+			return false
+		}
+	}
+	return false
 }
 
 func (tree *AVLTree) Insert(curr *TreeNode, x int) {
+
 	if curr == nil {
-		fmt.Println("insert <nul>");
-		tree.root = &TreeNode{data : x}
+		fmt.Println("insert <nul>")
+		tree.root = &TreeNode{data: x}
 		curr = tree.root
-		fmt.Println(tree.root);
-	} else if curr.data > x {
+		fmt.Println(tree.root)
+	} else if curr.GreaterThan(x) {
 		if curr.left == nil {
-			curr.left = &TreeNode{data : x}
-			if height(curr.left) - height(curr.right)  > 1 {
-				if x < curr.left.data {
-					case1(curr);
+			curr.left = &TreeNode{data: x}
+			if height(curr.left)-height(curr.right) > 1 {
+				if curr.left.GreaterThan(x) {
+					case1(curr)
 				} else {
-					case2(curr);
+					case2(curr)
 				}
 			}
 		} else {
 			tree.Insert(curr.left, x)
 		}
-	} else if curr.data < x {
+	} else if curr.LesserThan(x) {
 		if curr.right == nil {
-			curr.right = &TreeNode{data : x}
-			if height(curr.right) - height(curr.left)  > 1 {
-				if x > curr.right.data {
-					case4(curr);
+			curr.right = &TreeNode{data: x}
+			if height(curr.right)-height(curr.left) > 1 {
+				if curr.right.LesserThan(x) {
+					case4(curr)
 				} else {
-					case3(curr);
+					case3(curr)
 				}
 			}
 		} else {
@@ -78,64 +236,64 @@ func (tree *AVLTree) Insert(curr *TreeNode, x int) {
 		return
 	}
 	tree.size++
-	curr.height = max(height(curr.left), height(curr.right))+1;
+	curr.height = max(height(curr.left), height(curr.right)) + 1
 }
 
 //prints the values in the AVLTree in order
-func (tree *AVLTree) Inorder(node *TreeNode){
+func (tree *AVLTree) Inorder(node *TreeNode) {
 	if node == nil {
-		return;
+		return
 	}
-	tree.Inorder(node.left);
-	fmt.Println(node.data); // TODO: getID works but not what we want
-	tree.Inorder(node.right);
+	tree.Inorder(node.left)
+	fmt.Println(node.data) // TODO: getID works but not what we want
+	tree.Inorder(node.right)
 }
 
 func (tree *AVLTree) Exists(curr *TreeNode, x int) bool {
 	if curr == nil {
-		fmt.Println("<nul>");
+		fmt.Println("<nul>")
 		return false
 	}
-	if curr.data > x{
+	if curr.GreaterThan(x) {
 		return tree.Exists(curr.left, x)
-	} else if curr.data < x {
+	} else if curr.LesserThan(x) {
 		return tree.Exists(curr.right, x)
-	} 
-	return true;
+	}
+	return true
 }
 
 //left left rotation
-func case1(k2 *TreeNode){
-	k1 := k2.left;
-	k2.left = k1.right;
-	k1.right = k2;
-	k2.height = max(height(k2.left), height(k2.right)) + 1;
-	k1.height = max(height(k1.left), k2.height) + 1;
+func case1(k2 *TreeNode) {
+	k1 := k2.left
+	k2.left = k1.right
+	k1.right = k2
+	k2.height = max(height(k2.left), height(k2.right)) + 1
+	k1.height = max(height(k1.left), k2.height) + 1
 
-	k2 = k1;
+	k2 = k1
 }
 
 //right right rotation
-func case4(k1 *TreeNode){
-	k2 := k1.right;
-	k1.right = k2.left;
-	k2.left = k1;
-	k1.height = max(height(k1.left), height(k1.right)) + 1;
-	k2.height = max(height(k2.left), k1.height) + 1;
+func case4(k1 *TreeNode) {
+	k2 := k1.right
+	k1.right = k2.left
+	k2.left = k1
+	k1.height = max(height(k1.left), height(k1.right)) + 1
+	k2.height = max(height(k2.left), k1.height) + 1
 
-	k1 = k2;
+	k1 = k2
 }
 
 //left right rotation
-func case2(k3 *TreeNode){
-	case4(k3.left);
-	case1(k3);
+func case2(k3 *TreeNode) {
+	case4(k3.left)
+	case1(k3)
 }
 
 //right left rotation
-func case3(k1 *TreeNode){
-	case1(k1.right);
-	case4(k1);
+func case3(k1 *TreeNode) {
+	case1(k1.right)
+	case4(k1)
 }
 
 func main() {
@@ -167,7 +325,7 @@ func main() {
 
 		if strings.Compare("q", text) != 0 {
 			i, _ := strconv.Atoi(text)
-			fmt.Println(tree.Exists(tree.root, i));
+			fmt.Println(tree.Exists(tree.root, i))
 		} else {
 			break
 		}
